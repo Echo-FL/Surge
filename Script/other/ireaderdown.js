38cf17a01bedb75f414f49d5edc1ea793eb8e677
sky003-Max <xinghongdao003@gmail.com>
2020-03-19 18:23:08 +0800

    fix

--- Script/other/ireaderdown.js
+++ Script/other/ireaderdown.js
@@ -13,5 +13,9 @@ $done({body: JSON.stringify(obj)});
 美区掌阅
 ^https:\/\/api\.ireader\.mobi\/download\/fee_tpl\/.* url script-response-body NobyDa/Surge/JS/ireaderdown.js
 
+surge 4
+
+http-response ^https:\/\/api\.ireader\.mobi\/download\/fee_tpl\/.* requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/sky003-Max/surge-personal/master/Script/other/ireaderdown.js
+
 Mimt=api.ireader.mobi
 */

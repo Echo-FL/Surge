5576fe76a5e999975ac2d61536440ee8c8c8a7d6
sky003-Max <xinghongdao003@gmail.com>
2020-05-14 17:48:24 +0800

    add thisweek

--- Script/self-js/Chronograph.js
+++ Script/self-js/Chronograph.js
@@ -35,5 +35,54 @@ if(minute===0)
    time='⌚️现在是'+judge+hour+'点整';
 else
    time= '⌚️现在是'+judge+hour+'点'+minute+"分";
-$notification.post( today , time ,"");
+var date = new Date() //当前时间
+var year = date.getFullYear()
+var month = date.getMonth()
+var day1= date.getDate(); //月份的获取与赋值范围为 0-11
+thisweek="当前是第" + getWeekNumber(year, month, day1) + "周"
+$notification.post( today , time ,thisweek);
+
+function isLeapYear(year) {
+    return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+}
+function getMonthDays(year, month) {
+    return [
+        31,
+        null,
+        31,
+        30,
+        31,
+        30,
+        31,
+        31,
+        30,
+        31,
+        30,
+        31
+    ][month
+    ] || (isLeapYear(year) ? 29 : 28);
+}
+function getWeekNumber(y, m, d) {
+    var now = new Date(y, m, d),
+        year = now.getFullYear(),
+        month = now.getMonth(),
+        days = now.getDate();
+    //那一天是那一年中的第多少天
+    for (var i = 0; i < month; i++) {
+        days += getMonthDays(year, i);
+    }
+    //那一年第一天是星期几
+    var yearFirstDay = new Date(year,
+        0,
+        1).getDay() || 7;
+    var week = null;
+    if (yearFirstDay == 1) {
+        week = Math.ceil(days / yearFirstDay);
+    } else {
+        days -= (7 - yearFirstDay + 1);
+        week = Math.ceil(days / 7) + 1;
+    }
+    return week - 7; //令2.17为第一周的周一
+}
+//成功判断当前周数
 $done();
